5210fdc92dc0826b7b4ae74fc30a1aa91c435562
Henry03-dev <85604492+Henry03-dev@users.noreply.github.com>
2025-04-03 14:10:18 +0000

    0.0.3

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -7,6 +7,31 @@ void main() {
 
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        // 기본 폰트 설정
+        fontFamily: 'sunflower',
+
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 80.0,
+            fontFamily: 'parisienne',
+          ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
--- myapp/lib/screen/home_screen.dart
+++ myapp/lib/screen/home_screen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
@@ -17,64 +18,69 @@ class HomeScreen extends StatelessWidget {
           width: MediaQuery.of(context).size.width, // 현재 앱의 화면의 최대 너비(나누기 /를 통해 화면 분할도 가능)
           child: Column(
             children: [
-              Expanded(
-                child: Container(
-                  child: Column(
-                    children: [
-                      Text(
-                        'U&I',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'parisienne',
-                          fontSize: 80.0,
-                        ),
-                      ),
-                      Text(
-                        '우리 처음 만난날',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontSize: 30.0,
-                        ),
-                      ),
-                      Text(
-                        '2023.11.23',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      IconButton(
-                        iconSize: 60.0,
-                        color: Colors.red,
-                        onPressed: (){}, 
-                        icon: Icon(
-                          Icons.favorite,
-                        ),
-                      ),
-                      Text(
-                        'D+1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'sunflower',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 50.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  child: Image.asset(
-                    'asset/img/middle_image.png',
-                  ),
-                ),
-              ),
+              _Top(),
+              _Bottom(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Top extends StatelessWidget {
+  const _Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Text(
+              'U&I',
+              style: textTheme.displayLarge,
+            ),
+            Text(
+              '우리 처음 만난날',
+               style: textTheme.bodyLarge,
+            ),
+            Text(
+              '2023.11.23',
+              style: textTheme.bodyMedium,
+            ),
+            IconButton(
+              iconSize: 60.0,
+              color: Colors.red,
+              onPressed: (){
+                
+              }, 
+              icon: Icon(
+                Icons.favorite,
+              ),
+            ),
+            Text(
+              'D+1',
+              style: textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset(
+          'asset/img/middle_image.png',
         ),
       ),
     );

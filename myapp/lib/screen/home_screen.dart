6d7f4407ccd37de8fa28741ad49b73df7e5e9c51
Henry03-dev <85604492+Henry03-dev@users.noreply.github.com>
2025-04-09 13:48:24 +0000

    0.1.1

--- myapp/lib/screen/home_screen.dart
+++ myapp/lib/screen/home_screen.dart
@@ -2,12 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
@@ -16,10 +22,16 @@ class HomeScreen extends StatelessWidget {
         left: false,
         right: false,
         child: SizedBox(
-          width: MediaQuery.of(context).size.width, // 현재 앱의 화면의 최대 너비(나누기 /를 통해 화면 분할도 가능)
+          width: MediaQuery.of(context)
+              .size
+              .width, // 현재 앱의 화면의 최대 너비(나누기 /를 통해 화면 분할도 가능)
           child: Column(
             children: [
-              _Top(),
+              _Top(
+                selectedDate: selectedDate,
+
+                onPressed: onHeartPressed,
+              ),
               _Bottom(),
             ],
           ),
@@ -27,18 +39,55 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.white,
+            height: 300.0,
+            width: MediaQuery.of(context).size.width,
+            child: CupertinoDatePicker(
+              // date 기준으로 날짜 선택 가능하게 정렬
+              mode: CupertinoDatePickerMode.date,
+              // 한번 설정한 날짜를 저장
+              initialDateTime: selectedDate,
+              // 날짜 초과 선택 방지
+              maximumDate: DateTime.now(),
+              // 한국 날짜 서순에 맞게 ymd 정렬
+              dateOrder: DatePickerDateOrder.ymd,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
 
 class _Top extends StatefulWidget {
-  const _Top({super.key});
+  final DateTime selectedDate;
+  final VoidCallback? onPressed;
+
+  const _Top({
+    required this.selectedDate,
+    required this.onPressed,
+    super.key,
+  });
 
   @override
   State<_Top> createState() => _TopState();
 }
 
 class _TopState extends State<_Top> {
-  DateTime selectedDate = DateTime.now();
-
   @override
   Widget build(BuildContext context) {
     final now = DateTime.now();
@@ -54,49 +103,24 @@ class _TopState extends State<_Top> {
             ),
             Text(
               '우리 처음 만난날',
-               style: textTheme.bodyLarge,
+              style: textTheme.bodyLarge,
             ),
             Text(
-              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+              '${widget.selectedDate.year}.${widget.selectedDate.month}.${widget.selectedDate.day}',
               style: textTheme.bodyMedium,
             ),
             IconButton(
               iconSize: 60.0,
               color: Colors.red,
-              onPressed: (){
-                showCupertinoDialog(
-                  context: context, 
-                  barrierDismissible: true,
-                  builder: (BuildContext context) {
-                    return Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        color: Colors.white,
-                        height: 300.0,
-                        width: MediaQuery.of(context).size.width,
-                        child: CupertinoDatePicker(
-                          // date 기준으로 날짜 선택 가능하게 정렬
-                          mode: CupertinoDatePickerMode.date,
-                          // 한국 날짜 서순에 맞게 ymd 정렬
-                          dateOrder: DatePickerDateOrder.ymd,
-                          onDateTimeChanged: (DateTime date) {
-                            setState(() {
-                              selectedDate = date;
-                            });
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                );
-              }, 
+              onPressed: widget.onPressed,
               icon: Icon(
                 Icons.favorite,
               ),
             ),
+
             Text(
               // .inDays => 일 수로 변환
-              'D+${now.difference(selectedDate).inDays + 1}',
+              'D+${now.difference(widget.selectedDate).inDays + 1}',
               style: textTheme.displayMedium,
             ),
           ],
